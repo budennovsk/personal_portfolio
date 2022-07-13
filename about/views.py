67b588e54c2ee6953e16ad9f2370276ad187b57a
luka <svetloewill@mail.ru>
2022-07-13 18:31:05 +0300

    First commit!

---
+++ about/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import About
+
+def all_about(request):
+    abouts = About.objects.all()
+    return render(request, 'about/all_about.html', {'abouts':abouts})
---
+++ blog/__init__.py
@@ -0,0 +1,2 @@
+def site():
+    return None

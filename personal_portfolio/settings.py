48e103d931e453c723a4bb363c18fbf6a7921861
Roman <roma@mail.ru>
2022-10-06 16:05:54 +0300

    переделал старый саит 02.09.2022

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('DB_URL')
+SECRET_KEY = 'regrtgygtyhutjtj'
 
 
 

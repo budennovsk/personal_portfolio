2bd62e608f80e583c64b65f62315024fdd992035
Roman <roma@mail.ru>
2022-10-06 17:21:10 +0300

    переделал старый саит 02.09.2022

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = 'regfvfgvfg34VRErtgygtyhutjtj'
+load_dotenv()
+SECRET_KEY = os.getenv('DB_URL')
 
 
 
